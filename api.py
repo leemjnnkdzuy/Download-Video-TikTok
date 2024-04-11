8e74f9cf97da4659b2a39b1955d66291e2b5c11b
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-04-11 16:14:00 +0700

    update func.py

--- api.py
+++ api.py
@@ -6,8 +6,8 @@ def API(link):
     querystring = {"url": link}
 
     headers = {
-        "X-RapidAPI-Key": "",
-        "X-RapidAPI-Host": ""
+        "X-RapidAPI-Key": "9cf5a71810msh1694cf5276f68f3p15199cjsn34794d661141",
+        "X-RapidAPI-Host": "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com"
     }
 
     response = requests.get(url, headers=headers, params=querystring)
